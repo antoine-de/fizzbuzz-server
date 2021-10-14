19d95d8a601bee963301eb42ffd6a46e5532dc44
antoine-de <antoine.desbordes@gmail.com>
2021-10-14 15:11:21 +0200

    bind locally

--- src/bin/fizzbuzz_server.rs
+++ src/bin/fizzbuzz_server.rs
@@ -3,7 +3,7 @@ use actix_web::{App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(fizzbuzz_server::fizzbuzz_api))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
