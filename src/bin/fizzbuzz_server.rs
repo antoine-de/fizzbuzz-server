b09584003dc17fc94d5076345039778449595b4c
antoine-de <antoine.desbordes@gmail.com>
2021-10-14 15:12:00 +0200

    add logger

--- src/bin/fizzbuzz_server.rs
+++ src/bin/fizzbuzz_server.rs
@@ -2,6 +2,11 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    if std::env::var("RUST_LOG").is_err() {
+        // default log level is info
+        std::env::set_var("RUST_LOG", "info");
+    }
+    pretty_env_logger::init();
     HttpServer::new(|| App::new().service(fizzbuzz_server::fizzbuzz_api))
         .bind("0.0.0.0:8080")?
         .run()
